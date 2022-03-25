440a7dd65ea55c47a026637cb764b01985d907da
monkin77 <monkinsmurf@gmail.com>
2022-03-25 21:06:25 +0000

    Finished logic that counts the fins endGame

--- 2semestre/IA/tp1/connect4.py
+++ 2semestre/IA/tp1/connect4.py
@@ -12,20 +12,24 @@ state = {
 
 NUM_CONSECUTIVE = 4
 
+
 def showBoard():
     for row in state["board"]:
         print(row)
     print()
+
 
 def showState():
     showBoard()
     print("Player's turn: ", state["turn"])
     print()
 
+
 def isValidCell(col):
     if col < 0 or col >= BOARD_NUM_COLS:
         return False
     return state["board"][0][col] == EMPTY_CELL
+
 
 def calculateRowFromCol(col):
     for i in range(BOARD_NUM_ROWS - 1, 0, -1):
@@ -33,19 +37,24 @@ def calculateRowFromCol(col):
             return i
     return -1
 
+
 def getPlayerInput():
     while True:
-        col =  int( input("Player " + str(state["turn"]) + ", choose The Column\n") )
+        col = int(
+            input("Player " + str(state["turn"]) + ", choose The Column\n"))
         if isValidCell(col):
             row = calculateRowFromCol(col)
             return (row, col)
         print("That's an invalid input!")
+
 
 def make_turn(row, col):
     state["board"][row][col] = P1_CELL if state["turn"] == 1 else P2_CELL
 
 # numPieces -> number of pieces trying to detect in a row
 # Returns -> Number of horizontal solutions for a cell
+
+
 def checkHorizontal(row, col, player, numPieces):
     piecesCount = 0
     solCount = 0
@@ -74,6 +83,8 @@ def checkHorizontal(row, col, player, numPieces):
 
 # numPieces -> number of pieces trying to detect in a row
 # Returns -> Number of horizontal solutions for a cell
+
+
 def checkVertical(row, col, player, numPieces):
     piecesCount = 0
     solCount = 0
@@ -101,6 +112,30 @@ def checkVertical(row, col, player, numPieces):
     return solCount
 
 
+"""
+# numPieces -> number of pieces trying to detect in a row
+# Returns -> Number of horizontal solutions for a cell
+"""
+def checkDiagonal(row, col, player, numPieces):
+    solCount = 0
+
+    directions = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+    for direction in directions:
+        piecesCount = 0
+        for i in range(numPieces):
+            newRow = row + direction[0] * i
+            newCol = col + direction[1] * i
+            if (newRow < BOARD_NUM_ROWS) and (newRow >= 0) and (newCol >= 0) and (newCol < BOARD_NUM_COLS):
+                if state["board"][newRow][newCol] == player:
+                    piecesCount += 1
+                else:
+                    break
+        if piecesCount == numPieces:
+            solCount += 1
+
+    return solCount
+
+
 def endGame():
     # DETECT ENDGAME
     for i in range(BOARD_NUM_ROWS):
@@ -108,6 +143,8 @@ def endGame():
             if checkHorizontal(i, j, state["turn"], NUM_CONSECUTIVE):
                 return True
             if checkVertical(i, j, state["turn"], NUM_CONSECUTIVE):
+                return True
+            if checkDiagonal(i, j, state["turn"], NUM_CONSECUTIVE):
                 return True
 
     return False
